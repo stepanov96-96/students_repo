cf9524a91b7c02a41cec6798d0dcc76dddf696ad
Owerch <sasanikonenko53874@gmail.com>
2024-02-10 19:46:52 +0700

    Задания

---
+++ 2.py
@@ -0,0 +1,14 @@
+print("Назовите первое число")
+a = int(input())
+print("Назовите второе число")
+b = int(input())
+d1 = a + b
+d2 = a - b
+d3 = a * b
+d4 = a / b
+d5 = a % b
+print(f"Сложение:{d1}")
+print(f"Вычетание:{d2}")
+print(f"Умножение:{d3}")
+print(f"Деление:{d4}")
+print(f"Остаток:{d5}")
---
+++ 3.py
@@ -0,0 +1,6 @@
+import math
+
+print("Введите радиус круга")
+R = int(input())
+S = math.pi * math.sqrt(R)
+print(f"Площадь равны: {S}")
