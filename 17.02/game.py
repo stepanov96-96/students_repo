a1237451e7a633dc71fd6545f4de56cd54a3f341
Denis Salomykov <nikto1020@mail.ru>
2024-02-17 16:25:44 +0700

    fix_prog-rabotaet-kaknado

--- 17.02/game.py
+++ 17.02/game.py
@@ -3,11 +3,17 @@ import random
 def privet():
     nick = input("Введите ник: ")
     print(f"Привет, {nick}! В этой игре тебе нужно угадать число, вводя цифры.")
+    
+noll = 0
 
 def menu():
+    global noll
+    print("---------------------")
     print("1. Start")
     print("2. Pravila")
-    print("3. Exit")
+    print("3. Record")
+    print("4. Exit")
+    print("---------------------")
     
     a = input()
     if a == '1' or a.lower() == "старт" or a.lower() == "start":
@@ -20,14 +26,17 @@ def menu():
             menu()
         else:
             menu()
-    elif a == '3' or a.lower() == "e":
+    elif a == "3":
+        print(noll)
+        menu()
+    elif a == '4' or a.lower() == "e":
         print("Пока :(")
 
 def guess_number():
+    global noll
     number = str(random.randint(1000, 9999))
     attempts = 0
     life = 3
-    noll = 0
     
     print("Я загадал число, попробуй угадать в нем одну цифру!")
     while life > 0:
@@ -35,19 +44,21 @@ def guess_number():
         if guess.isdigit() and len(guess) == 1:
             attempts += 1
             if guess in number:
-                print("Ты угадал цифру!")
-                noll += 1
+                print("Ты угадал цифру!", end="\n\n")
+                noll += 10
+                if noll == len(number) * 10:
+                    print(f"Поздравляю, ты угадал все цифры числа {number} за {attempts} попыток! Очки: {noll}")
+                    break
             else:
-                print("Промах, давай еще.")
+                print("Промах, давай еще.", end="\n\n")
                 life -= 1
                 print(f"У тебя осталось {life} жизней.")
         else:
             print("Введи ОДНУ цифру и не букву")
-    
+
     if life == 0:
         print(f"Ты проиграл :( Загаданное число было: {number}")
-    else:
-        print(f"Поздравляю, ты угадал все цифры числа {number} за {attempts} попыток! Очки: {noll}")
+    menu()
 
 privet()
 menu()
