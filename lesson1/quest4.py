409abc4d329a0bd97c359808ec5699b2d9dd53a3
loonwy <157209226+loonwy@users.noreply.github.com>
2024-03-16 06:29:05 +0300

    Update quest4.py

--- lesson1/quest4.py
+++ lesson1/quest4.py
@@ -1,4 +1,9 @@
 num1 = input('Введите число №1: ')
 num2 = input('Введите число №2: ')
-print(int(num1) + int(num2))
-print(float(num1) + float(num2))
+s = int(num1) + int(num2)
+try:
+    int(s)
+    return True
+except ValueError:
+    return False
+print (s)
